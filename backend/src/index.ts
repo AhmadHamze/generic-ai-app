d3cef02e0c0802f292dcc8601ec943b529cddcaf
Ahmad Hamze <ahmadhamze@yahoo.com>
2025-09-06 18:27:03 +0300

    Modify messages endpoint and add cors middleware.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { logger } from "./middlewares/logging";
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(logger);
 
 app.get("/hello", (_, res) => {
@@ -12,7 +14,7 @@ app.get("/hello", (_, res) => {
 
 app.get("/messages{/:id}", (req, res) => {
   if (!req.params.id) {
-    return res.json(fakeData.messages);
+    return res.json(Object.values(fakeData.messages));
   }
   const message = fakeData.messages[req.params.id];
   if (message) {
@@ -43,6 +45,26 @@ const fakeData = Object.freeze({
       id: "2",
       question: "How are you?",
       answer: "I'm fine, thank you!",
+    },
+    "3": {
+      id: "3",
+      question: "What is 2 + 2?",
+      answer: "4",
+    },
+    "4": {
+      id: "4",
+      question: "What is the capital of Germany?",
+      answer: "Berlin",
+    },
+    "5": {
+      id: "5",
+      question: "What is the capital of Italy?",
+      answer: "Rome",
+    },
+    "6": {
+      id: "6",
+      question: "What is the capital of Spain?",
+      answer: "Madrid",
     },
   } as { [key: string]: Message },
 });
